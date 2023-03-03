c03a7ecc475479ffe88b97e71123878d7e8a2755
Harasymiv <vira.harasymiv@nung.edu.ua>
2023-03-04 01:54:28 +0200

    HardwareCartTests.java changed

---
+++ src/test/java/edu/IFNTUNG/bpbonline/cartTests/hardwareCartTests/HardwareCartTests.java
@@ -0,0 +1,35 @@
+package edu.IFNTUNG.bpbonline.cartTests.hardwareCartTests;
+
+import edu.IFNTUNG.bpbonline.application.pages.HomePage;
+import edu.IFNTUNG.bpbonline.application.pages.ProductItemPage;
+import edu.IFNTUNG.bpbonline.asserts.Asserts;
+import edu.IFNTUNG.bpbonline.runners.BaseTest;
+import edu.IFNTUNG.bpbonline.utils.CsvDataProviders;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+/**
+ * This class contains user's automated steps for checking
+ * the price of the hardware's items in the cart.
+ *
+ * @author Vira Harasymiv
+ */
+public class HardwareCartTests extends BaseTest {
+    @Test(dataProvider = "csvReader",dataProviderClass = CsvDataProviders.class)
+    public void checkItemPriceInCart(Map<String,String> testData){
+        String number = testData.get("number");
+        String itemCount = testData.get("itemCount");
+        log.info("Hardware Cart Tests #" + number);
+        String itemPrice = new HomePage(driver,log)
+                .clickOnHardwareMenu()
+                .clickOnFirstProductItem()
+                .getProductPrice();
+        String actualTotalPrice = new ProductItemPage(driver,log)
+                .clickOnAddToCartButton()
+                .typeItemsQuantity(itemCount)
+                .clickOnUpdateButton(itemCount)
+                .getTotalPriceInCart();
+        Asserts.checkPriceInCart(actualTotalPrice,itemPrice,itemCount);
+    }
+}
