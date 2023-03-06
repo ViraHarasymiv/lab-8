472ee43303521a11d9696e825408634f64da2976
Harasymiv <vira.harasymiv@nung.edu.ua>
2023-03-06 15:54:05 +0200

    monochrome added

--- src/test/java/edu/IFNTUNG/bpbonline/tests/RunCucumberTest.java
+++ src/test/java/edu/IFNTUNG/bpbonline/tests/RunCucumberTest.java
@@ -6,6 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/resources/cucumber.features", plugin = {
         "pretty", "html:target/cucumber-report/report.html",},
         glue = "edu/IFNTUNG/bpbonline/application/stepdefinitions",
-        tags = "@checkItemPriceInCart or @CreateAccountTest")
+        tags = "@checkItemPriceInCart or @CreateAccountTest",
+        monochrome = true)
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 }
