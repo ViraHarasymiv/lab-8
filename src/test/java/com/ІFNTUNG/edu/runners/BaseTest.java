2996cfadfdc9f05c987bfa0c38dfd23830ea4e07
Harasymiv <vira.harasymiv@nung.edu.ua>
2023-03-14 20:32:04 +0200

    testng.xml files added

--- src/test/java/com/ІFNTUNG/edu/runners/BaseTest.java
+++ src/test/java/com/ІFNTUNG/edu/runners/BaseTest.java
@@ -1,29 +1,36 @@
 package com.ІFNTUNG.edu.runners;
 
+import com.ІFNTUNG.edu.utils.SauceLabsTestListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
 
+@Listeners({SauceLabsTestListener.class})
 public class BaseTest {
     protected WebDriver driver;
     protected Logger log;
 
-    @Parameters({ "browser", "environment" })
+    @Parameters({ "browser", "environment", "platform"})
     @BeforeMethod(alwaysRun = true)
-    public void setUp(Method method, @Optional("chrome") String browser, @Optional("local") String environment, ITestContext ctx) {
+    public void setUp(Method method, @Optional("chrome") String browser, @Optional("local") String environment,  @Optional("Windows 10") String platform, ITestContext ctx) {
         log = LogManager.getLogger(ctx.getCurrentXmlTest().getSuite().getName());
-
+        ctx.setAttribute("sauce",false);
         switch (environment) {
 
             case "grid":
                 driver = new GridFactory(browser, log).createDriver();
+                break;
+
+            case "sauce":
+                ctx.setAttribute("sauce",true);
+                String sauceTestName = ctx.getName() + " | " + method.getName() + " | " + browser + " | " + platform;
+                SauceLabsFactory factory = new SauceLabsFactory(browser, platform, log, sauceTestName);
+                driver = factory.createDriver();
+                ctx.setAttribute("sessionId",factory.getSessionId());
                 break;
 
             default:
