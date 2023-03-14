72a6e699803d5408a72717c2e05aff1060292dac
Harasymiv <vira.harasymiv@nung.edu.ua>
2023-03-14 14:35:15 +0200

    runners added

---
+++ src/test/java/com/ІFNTUNG/edu/runners/BaseTest.java
@@ -0,0 +1,42 @@
+package com.ІFNTUNG.edu.runners;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.lang.reflect.Method;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected Logger log;
+
+    @Parameters({ "browser", "environment" })
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(Method method, @Optional("chrome") String browser, @Optional("local") String environment, ITestContext ctx) {
+        log = LogManager.getLogger(ctx.getCurrentXmlTest().getSuite().getName());
+
+        switch (environment) {
+
+            case "grid":
+                driver = new GridFactory(browser, log).createDriver();
+                break;
+
+            default:
+                driver = new BrowserDriverFactory(browser, log).createDriver();
+                break;
+        }
+
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        log.info("Close driver");
+        driver.quit();
+    }
+}
